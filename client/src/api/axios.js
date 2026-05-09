8028605ddec972ecf1fc4d57002094c2b5184821
Prakhar7755 <prakhar.5779.aps3@gmail.com>
2026-05-09 22:22:43 +0530

    refactor: update .env.sample

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_MODE === 'development' ? `http://localhost:5001/api` : '/api';
+const BASE_URL =
+  import.meta.env.VITE_MODE === 'development' ? import.meta.env.VITE_API_BASE_URL : '/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
